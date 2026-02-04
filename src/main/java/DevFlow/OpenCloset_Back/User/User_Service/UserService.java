a23bb86b12aff712d8b198d72a0990517764bf32
oxoxoxoxox <dudwlsaos132@naver.com>
2026-02-04 15:24:57 +0900

    edit : 닉네임 중복 처리 수정

--- src/main/java/DevFlow/OpenCloset_Back/User/User_Service/UserService.java
+++ src/main/java/DevFlow/OpenCloset_Back/User/User_Service/UserService.java
@@ -21,6 +21,11 @@ public class UserService {
             throw new IllegalArgumentException("이미 사용 중인 이메일입니다.");
         }
 
+        // 닉네임 중복 체크
+        if (userRepository.findByNickname(requestDto.getNickname()).isPresent()) {
+            throw new IllegalArgumentException("이미 사용 중인 닉네임입니다.");
+        }
+
         // 비밀번호 암호화
         String encryptedPassword = passwordEncoder.encode(requestDto.getPassword());
 
