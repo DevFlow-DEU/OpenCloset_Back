f6015826c406eea3efa4f21b1c0c889333d7daa1
oxoxoxoxox <dudwlsaos132@naver.com>
2026-03-05 21:39:58 +0900

    fix : 각 의류에 대한 Repository 파일 수정

--- src/main/java/DevFlow/OpenCloset_Back/Board/Repository/BottomRepository.java
+++ src/main/java/DevFlow/OpenCloset_Back/Board/Repository/BottomRepository.java
@@ -1,9 +1,13 @@
 package DevFlow.OpenCloset_Back.Board.Repository;
 
+import DevFlow.OpenCloset_Back.Board.entity.Board;
 import DevFlow.OpenCloset_Back.Board.entity.Bottom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BottomRepository extends JpaRepository<Bottom, Long> {
+    void deleteByBoardIn(List<Board> boards);
 }
