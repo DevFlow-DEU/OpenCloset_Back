bb0d95f62b2d89530cdb534d37808d9faa09431f
oxoxoxoxox <dudwlsaos132@naver.com>
2026-03-11 19:32:10 +0900

    chore : 회원탈퇴에 대한 스웨거 수정

--- src/main/java/DevFlow/OpenCloset_Back/User/User_Controller/UserDeleteController.java
+++ src/main/java/DevFlow/OpenCloset_Back/User/User_Controller/UserDeleteController.java
@@ -1,6 +1,12 @@
 package DevFlow.OpenCloset_Back.User.User_Controller;
 
 import DevFlow.OpenCloset_Back.User.User_Service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -13,10 +19,16 @@ import java.util.Map;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
+@Tag(name = "회원 탈퇴", description = "회원 탈퇴 API")
 public class UserDeleteController {
 
     private final UserService userService;
 
+    @Operation(summary = "회원 탈퇴", description = "현재 로그인된 사용자의 계정을 삭제합니다. 관련된 게시글 및 데이터도 함께 삭제됩니다. JWT 토큰이 필요합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "회원 탈퇴 성공", content = @Content(mediaType = "application/json", examples = @ExampleObject(value = "{\"message\": \"회원탈퇴가 완료되었습니다.\"}"))),
+            @ApiResponse(responseCode = "401", description = "인증되지 않은 사용자 (토큰 없음 또는 만료)", content = @Content(mediaType = "application/json", examples = @ExampleObject(value = "{\"message\": \"Unauthorized\"}")))
+    })
     @DeleteMapping("/delete")
     public ResponseEntity<?> deleteUser(Authentication authentication) {
         String email = authentication.getName();
