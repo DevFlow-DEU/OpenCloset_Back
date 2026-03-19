a6e12867d327b07968f277cdc8b0097e08ed940f
oxoxoxoxox <dudwlsaos132@naver.com>
2026-03-19 13:45:53 +0900

    feat : 로그아웃 API 마이페이지 Controller 로 변경

--- src/main/java/DevFlow/OpenCloset_Back/Login/Login_Controller/LoginController.java
+++ src/main/java/DevFlow/OpenCloset_Back/Login/Login_Controller/LoginController.java
@@ -4,7 +4,6 @@ import DevFlow.OpenCloset_Back.Login.Dto.req.LoginRequestDto;
 import DevFlow.OpenCloset_Back.Login.Dto.res.LoginResponseDto;
 import DevFlow.OpenCloset_Back.Login.Login_Service.LoginService;
 
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,11 +18,5 @@ public class LoginController {
     public ResponseEntity<LoginResponseDto> loginUser(@RequestBody LoginRequestDto requestDto) {
         LoginResponseDto responseDto = loginService.loginUser(requestDto);
         return ResponseEntity.ok(responseDto);
-    }
-
-    @PostMapping("/logout")
-    public ResponseEntity<String> logout(HttpSession session) {
-        loginService.logoutUser(session);
-        return ResponseEntity.ok("로그아웃 성공!");
     }
 }
