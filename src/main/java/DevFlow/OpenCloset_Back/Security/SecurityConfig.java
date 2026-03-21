c53fc760f95c707c80fa7dc0d9b16fe77931109d
oxoxoxoxox <dudwlsaos132@naver.com>
2026-03-21 22:42:07 +0900

    feat : 프로필 이미지 업로드 API 권한 설정

--- src/main/java/DevFlow/OpenCloset_Back/Security/SecurityConfig.java
+++ src/main/java/DevFlow/OpenCloset_Back/Security/SecurityConfig.java
@@ -68,7 +68,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/password-change", "/auth/delete").authenticated()
                         .requestMatchers("/", "/auth/register", "/auth/login", "/auth/logout", "/auth/refresh", "/search",
-                                "/api/**")
+                                "/api/**", "/uploads/**")
                         .permitAll()
                         .requestMatchers("/auth/**", "/auth/kakao/**", "/search", "/error").permitAll()
                         // Swagger UI 허용
