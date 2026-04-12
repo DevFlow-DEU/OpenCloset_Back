00cf5c0f632b9971e4c8af5309cb22f3dde1a934
oxoxoxoxox <dudwlsaos132@naver.com>
2026-04-12 12:57:47 +0900

    feat : 스웨거에서도 API 통신이 되도록 설정

--- src/main/java/DevFlow/OpenCloset_Back/Config/SwaggerConfig.java
+++ src/main/java/DevFlow/OpenCloset_Back/Config/SwaggerConfig.java
@@ -5,6 +5,8 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,12 +23,22 @@ public class SwaggerConfig {
                 .scheme("bearer")
                 .bearerFormat("JWT"));
 
+        Server prodServer = new Server();
+        prodServer.setUrl("https://opencloset.jihongeek.com");
+        prodServer.setDescription("운영 서버 (HTTPS)");
+
+        Server localServer = new Server();
+        localServer.setUrl("http://localhost:8080");
+        localServer.setDescription("로컬 테스트 서버 (HTTP)");
+
         return new OpenAPI()
                 .info(new Info()
                         .title("OpenCloset API")
                         .description("OpenCloset 백엔드 API 문서")
                         .version("v1.0.0"))
                 .addSecurityItem(securityRequirement)
-                .components(components);
+                .components(components)
+                .addServersItem(prodServer)
+                .addServersItem(localServer);
     }
 }
