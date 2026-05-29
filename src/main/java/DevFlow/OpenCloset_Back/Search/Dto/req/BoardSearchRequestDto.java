15d798acb44946be3e624d720500a3cff0c702e0
oxoxoxoxox <dudwlsaos132@naver.com>
2026-05-29 17:02:39 +0900

    feat : 게시물 검색 결과에 해당하는 req 파일 명세 작성 및 수정

--- src/main/java/DevFlow/OpenCloset_Back/Search/Dto/req/BoardSearchRequestDto.java
+++ src/main/java/DevFlow/OpenCloset_Back/Search/Dto/req/BoardSearchRequestDto.java
@@ -1,15 +1,32 @@
 package DevFlow.OpenCloset_Back.Search.Dto.req;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Schema(description = "게시물 검색 요청 DTO")
 public class BoardSearchRequestDto {
-    private String title;
-    private String description;
-    private String sex;
-    private String size;
-    private String place;
 
+    @Schema(description = "검색 키워드 (제목에서 검색)", example = "나이키")
+    private String title;
+
+    @Schema(description = "검색 키워드 (설명에서 검색)", example = "바람막이")
+    private String description;
+
+    @Schema(description = "성별 필터 (M / W / 공용)", example = "M")
+    private String sex;
+
+    @Schema(description = "사이즈 필터 (S / M / L / XL / FREE 등)", example = "L")
+    private String size;
+
+    @Schema(description = "카테고리 필터 (top / bottom / outer / one piece / jewelry / shoes / bag)", example = "outer")
+    private String category;
+
+    @Schema(description = "최소 가격 필터 (원)", example = "5000")
+    private Long minPrice;
+
+    @Schema(description = "최대 가격 필터 (원)", example = "30000")
+    private Long maxPrice;
 }
